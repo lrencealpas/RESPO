14e12a75582f31a58b312d0f15a37102859737b5
Kenneth Ayade <ayadekenneth07@gmail.com>
2023-12-02 22:13:29 +0800

    origin

--- src/main/java/com/respo/respo/Controller/TutorialsController.java
+++ src/main/java/com/respo/respo/Controller/TutorialsController.java
@@ -1,56 +1,75 @@
 package com.respo.respo.Controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import com.respo.respo.Entity.TutorialsEntity;
 import com.respo.respo.Service.TutorialsService;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tutorial")
-@CrossOrigin(origins = ("http://localhost:3000"))
+@CrossOrigin(origins = "http://localhost:3000")
 public class TutorialsController {
 
-	@Autowired
-	TutorialsService tserv;
-	
-	@GetMapping("/print")
-	public String itWorks() {
-		return "It works";
-	}
-	
-	//Create
-	@PostMapping("/insertTutorial")
-	public TutorialsEntity inserTutorial(@RequestBody TutorialsEntity tutorial) {
-		return tserv.insertTutorial(tutorial);
-	}
-	
-	//Read
-	@GetMapping("/getAllTutorials")
-	public List<TutorialsEntity> getAllTutorials() {
-		return tserv.getAllTutorials();
-	}
-	
-	//U - Update a tutorial record
-	@PutMapping("/updateUser")
-	public TutorialsEntity updateTutorial(@RequestParam int videoId, @RequestBody TutorialsEntity newTutorialDetails) {
-		return tserv.updateTutorial(videoId, newTutorialDetails);
-	}
-					
-	//D - Delete a tutorial record
-	@DeleteMapping("/deleteTutorial/{videoId}")
-	public String deleteTutorial(@PathVariable int videoId) {
-		return tserv.deleteTutorial(videoId);
-	}
+    @Autowired
+    TutorialsService tserv;
+
+    @Value("${video.upload.directory}") // Inject the directory path from application.properties
+    private String uploadDirectory;
+
+    @GetMapping("/print")
+    public String itWorks() {
+        return "It works";
+    }
+
+    // Create - Insert a tutorial with video content
+    @PostMapping("/insertTutorial")
+    public TutorialsEntity insertTutorial(@RequestParam("file") MultipartFile file,
+                                         @RequestParam("title") String title,
+                                         @RequestParam("desc") String desc) {
+        try {
+            TutorialsEntity tutorial = new TutorialsEntity();
+            tutorial.setTitle(title);
+            tutorial.setDesc(desc);
+
+            // Save video file in the specified directory
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(uploadDirectory + "/" + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            // Store the URL path to access the video
+            String videoUrl = "/videos/" + file.getOriginalFilename();
+            tutorial.setVideoUrl(videoUrl);
+
+            return tserv.insertTutorial(tutorial);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null; // Or handle the exception appropriately
+        }
+    }
+
+    // Read - Get all tutorials
+    @GetMapping("/getAllTutorials")
+    public List<TutorialsEntity> getAllTutorials() {
+        return tserv.getAllTutorials();
+    }
+
+    // Update - Update a tutorial record
+    @PutMapping("/updateTutorial/{videoId}")
+    public TutorialsEntity updateTutorial(@PathVariable int videoId,
+                                          @RequestBody TutorialsEntity newTutorialDetails) {
+        return tserv.updateTutorial(videoId, newTutorialDetails);
+    }
+
+    // Delete - Delete a tutorial record
+    @DeleteMapping("/deleteTutorial/{videoId}")
+    public String deleteTutorial(@PathVariable int videoId) {
+        return tserv.deleteTutorial(videoId);
+    }
 }
--- src/main/java/com/respo/respo/Entity/TutorialsEntity.java
+++ src/main/java/com/respo/respo/Entity/TutorialsEntity.java
@@ -5,61 +5,80 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tblTutorials")
 public class TutorialsEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int videoId;
-	@Column(name = "Title")
-	private String title;
-	@Column(name = "Description")
-	private String desc;
-	@Column(name = "Content")
-	private String content;
-	
-	public TutorialsEntity() {}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int videoId;
 
-	public TutorialsEntity(int videoId, String title, String desc, String content) {
-		super();
-		this.videoId = videoId;
-		this.title = title;
-		this.desc = desc;
-		this.content = content;
-	}
+    @Column(name = "Title")
+    private String title;
 
-	public int getVideoId() {
-		return videoId;
-	}
+    @Column(name = "Description")
+    private String desc;
 
-	public void setVideoId(int videoId) {
-		this.videoId = videoId;
-	}
+    @Lob
+    @Column(name = "Content", columnDefinition = "LONGBLOB")
+    private byte[] content; // Representing video content as byte array
 
-	public String getTitle() {
-		return title;
-	}
+    @Column(name = "VideoUrl") // Add a new field for video URL
+    private String videoUrl;
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public TutorialsEntity() {
+    }
 
-	public String getDesc() {
-		return desc;
-	}
+    public TutorialsEntity(int videoId, String title, String desc, byte[] content, String videoUrl) {
+        this.videoId = videoId;
+        this.title = title;
+        this.desc = desc;
+        this.content = content;
+        this.videoUrl = videoUrl;
+    }
 
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
+    // Getters and setters for all fields
 
-	public String getContent() {
-		return content;
-	}
+    public int getVideoId() {
+        return videoId;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setVideoId(int videoId) {
+        this.videoId = videoId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
 }
